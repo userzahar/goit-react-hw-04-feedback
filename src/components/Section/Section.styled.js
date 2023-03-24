f17186715896f7d183562fa405204c56e7f9f60e
userzahar <110730909+userzahar@users.noreply.github.com>
2023-03-25 00:06:31 +0200

    add styled

--- src/components/Section/Section.styled.js
+++ src/components/Section/Section.styled.js
@@ -1,6 +1,7 @@
 const { default: styled } = require('styled-components');
 
 export const SectionStyle = styled.section`
+  margin: 0 auto;
   width: 350px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
   padding: 20px;
